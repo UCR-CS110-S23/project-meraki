90c46d9f1ec53ffa7c828de6d163c60f6960fd4a
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-04-17 18:26:50 -0700

    Added code for displaying a player's move on the tic tac toe board (also toggles between players X and O)

--- Lab 2/main.js
+++ Lab 2/main.js
@@ -5,84 +5,98 @@ Hannah Bach - hbach003 */
 
 var score_x = 0;
 var score_o = 0;
+var player = "X";
 
 let board = [
-  ['', '', ''],
-  ['', '', ''],
-  ['', '', '']
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
 ];
 
-console.log(board)
+console.log(board);
 
 // const player = new Boolean(false); //Player X = false;
+function addMove(cl, xId, oId) {
+  if (!document.getElementById(xId) && !document.getElementById(oId)) {
+    console.log("bruh");
+    if (player == "X") {
+      console.log("enterX");
 
-var player = 'X';
+      let addHtml = '<h1 class="move" id="' + xId + '">X</h1>';
+      console.log(addHtml);
+      $(addHtml).appendTo(cl);
+      player = "O";
+    } else if (player == "O") {
+      console.log("enterO");
 
+      let addHtml = '<h1 class="move" id="' + oId + '">O</h1>';
+      console.log(addHtml);
+      $(addHtml).appendTo(cl);
+      player = "X";
+    }
+  }
+  console.log(player);
+}
 
-$(function() {
-  $(".one").click(function() {
-    console.log("clicked box one")
-    $('<h1 id="X">X</h1>').appendTo('.one');
-
-    
+$(function () {
+  $(".one").on("click", function () {
+    console.log("clicked box one");
+    addMove(".one", "X1", "O1");
   });
 
-  $(".two").click(function() {
-    console.log("clicked box two")
-    
+  $(".two").on("click", function () {
+    console.log("clicked box two");
+    addMove(".two", "X2", "O2");
   });
 
-  $(".three").click(function() {
-    console.log("clicked box three")
-    
+  $(".three").on("click", function () {
+    console.log("clicked box three");
+    addMove(".three", "X3", "O3");
   });
 
-  $(".four").click(function() {
-    console.log("clicked box four")
-    
+  $(".four").on("click", function () {
+    console.log("clicked box four");
+    addMove(".four", "X4", "O4");
   });
 
-  $(".five").click(function() {
-    console.log("clicked box five")
-    
+  $(".five").on("click", function () {
+    console.log("clicked box five");
+    addMove(".five", "X5", "O5");
   });
 
-  $(".six").click(function() {
-    console.log("clicked box six")
-    
+  $(".six").on("click", function () {
+    console.log("clicked box six");
+    addMove(".six", "X6", "O6");
   });
 
-  $(".seven").click(function() {
-    console.log("clicked box seven")
-    
+  $(".seven").on("click", function () {
+    console.log("clicked box seven");
+    addMove(".seven", "X7", "O7");
   });
 
-  $(".eight").click(function() {
-    console.log("clicked box eight")
-    
+  $(".eight").on("click", function () {
+    console.log("clicked box eight");
+    addMove(".eight", "X8", "O8");
   });
 
-  $(".nine").click(function() {
-    console.log("clicked box nine")
-    
+  $(".nine").on("click", function () {
+    console.log("clicked box nine");
+    addMove(".nine", "X9", "O9");
   });
-  
-} );
+});
 
-$(function() {
-  $(".new_game").click(function() {
-    console.log("create new game")
+$(function () {
+  $(".new_game").click(function () {
+    console.log("create new game");
     //scores should be kept
-    player = 'X';
+    player = "X";
+  });
+});
 
-  })
-})
-
-$(function() {
-  $(".reset").click(function() {
-    console.log("create new game")
+$(function () {
+  $(".reset").click(function () {
+    console.log("create new game");
     //scores should be reset too
-    player = 'X';
-
-  })
-})
+    player = "X";
+  });
+});
