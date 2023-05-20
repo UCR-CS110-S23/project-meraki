18b6371189180ddf737566b8d32e0f111c01dba4
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-05-19 17:46:00 -0700

    adding mongoDB stuff

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+console.log(process.env.mongodb);
+mongoose.connect(process.env.mongodb);
+
+const database = mongoose.connection;
+database.once("connected", () => {
+  console.log("Connected to the DB!");
+});
+// npm install mongoose
+// npm install dotenv
+//
