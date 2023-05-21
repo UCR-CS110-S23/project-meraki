b467a4ebc60cc49e912e3ab9c08bc5cbaa920ea1
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-05-20 21:04:18 -0700

    Edit Form was not displaying date and page of the books in book.json but it is now fixed. Also used MomentJS to parse the date from book.json. Lab should be now complete

--- Lab 5/book-list.js
+++ Lab 5/book-list.js
@@ -15,16 +15,24 @@ async function loadBooks() {
                     <div class="card">
                         <div class="card-body">
                             <h5 class = "card-title">${book.title}</h5>
-                            <h6 class = "card-subtitle mb-2 text-muted">${book.isbn}</h6>
+                            <h6 class = "card-subtitle mb-2 text-muted">${
+                              book.isbn
+                            }</h6>
 
                             <div>Author: ${book.author}</div>
                             <div>Publisher: ${book.publisher}</div>
-                            <div>Number Of Pages: ${book.numOfPages}</div>
+                            <div>Number Of Pages: ${
+                              book.numOfPages || book.pages
+                            }</div>
 
                             <hr>
 
-                            <button type="button" class="btn btn-danger" onClick="deleteBook(${book.isbn})">Delete</button>
-                            <button types="button" class="btn btn-primary" data-toggle="modal" data-target="#editBookModal" onClick="setEditModal(${book.isbn})">Edit</button>
+                            <button type="button" class="btn btn-danger" onClick="deleteBook(${
+                              book.isbn
+                            })">Delete</button>
+                            <button types="button" class="btn btn-primary" data-toggle="modal" data-target="#editBookModal" onClick="setEditModal(${
+                              book.isbn
+                            })">Edit</button>
                         </div>
                     </div>
                 </div>
@@ -50,14 +58,24 @@ async function setEditModal(isbn) {
     console.log("In setEditModal() ", data);
     const book = JSON.parse(data);
 
-    const { title, author, publisher, publish_date, numOfPages } = book;
+    let {
+      title,
+      author,
+      publisher,
+      publish_date,
+      numOfPages,
+      pages,
+      published,
+    } = book;
+
+    published = moment(published).utc().format("YYYY-MM-DD");
 
     document.getElementById("isbn").value = isbn;
     document.getElementById("title").value = title;
     document.getElementById("author").value = author;
     document.getElementById("publisher").value = publisher;
-    document.getElementById("publish_date").value = publish_date;
-    document.getElementById("numOfPages").value = numOfPages;
+    document.getElementById("publish_date").value = publish_date || published;
+    document.getElementById("numOfPages").value = numOfPages || pages;
 
     //TODO: EDIT POST METHOD FOR EDITING FORM. Currently does not display the updated data.
     document.getElementById(
