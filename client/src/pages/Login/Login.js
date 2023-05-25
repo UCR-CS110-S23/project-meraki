99649f5710a178b08eb1e72640a22512a4bbd253
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-05-25 03:19:39 -0700

    Combined the login and register form into its own form component called 'UserForm' since they both are similar. Register.js and Login.js pages now just return this UserForm component

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,42 +1,7 @@
-import { useState } from "react";
-import "./Login.css";
+import UserForm from "../../components/UserForm/UserForm";
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  return (
-    <div id="mainContainer">
-      <div id="loginContainer">
-        <h2>Log in</h2>
-        <div id="inputs">
-          <input
-            className="input"
-            id="nameInput"
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) =>
-              setUsername(e.target.value) || console.log(e.target.value)
-            }
-          ></input>
-          <input
-            className="input"
-            id="postContent"
-            type="text"
-            placeholder="Password"
-            value={password}
-            onChange={(e) =>
-              setPassword(e.target.value) || console.log(e.target.value)
-            }
-          ></input>
-        </div>
-        <div id="loginButtonContainer">
-          <button id="loginButton">Log in</button>
-        </div>
-      </div>
-    </div>
-  );
+  return <UserForm formType={"Log in"}></UserForm>;
 }
 
 export default Login;
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,42 +1,7 @@
-import { useState } from "react";
-import "./Register.css";
+import UserForm from "../../components/UserForm/UserForm";
 
 function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  return (
-    <div id="mainContainer">
-      <div id="registerContainer">
-        <h2>Register</h2>
-        <div id="inputs">
-          <input
-            className="input"
-            id="nameInput"
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) =>
-              setUsername(e.target.value) || console.log(e.target.value)
-            }
-          ></input>
-          <input
-            className="input"
-            id="postContent"
-            type="text"
-            placeholder="Password"
-            value={password}
-            onChange={(e) =>
-              setPassword(e.target.value) || console.log(e.target.value)
-            }
-          ></input>
-        </div>
-        <div id="registerButtonContainer">
-          <button id="registerButton">Register</button>
-        </div>
-      </div>
-    </div>
-  );
+  return <UserForm formType={"Register"}></UserForm>;
 }
 
 export default Register;
