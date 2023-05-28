24a99af804a680b7367954eaf22126a1c59b2172
Colliquake <aroh002@ucr.edu>
2023-05-28 13:08:17 -0700

    added profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,7 +1,32 @@
+import './Profile.css';
+
 function Profile() {
   return (
-    <div>
-      <h1>Profile</h1>
+    <div id="profileContent">
+      <h1 id="mainProfileText">Profile</h1>
+      <div id="searchBarBackground">
+        <div id="search">
+          <input id="searchBar" type="search" placeholder="Search Bar" size="50" />
+        </div>
+      </div>
+
+      <div id="postsBox">
+        <div id="profileText">
+          <h3>Your Posts</h3>
+        </div>
+        
+        <div id="profilePosts">
+          <div className="profilePost"></div>
+          <div className="profilePost"></div>
+          <div className="profilePost"></div>
+          <div className="profilePost"></div>
+          <div className="profilePost"></div>
+          <div className="profilePost"></div>
+          <div className="profilePost"></div>
+          <div className="profilePost"></div>
+
+        </div>
+      </div>
     </div>
   );
 }
