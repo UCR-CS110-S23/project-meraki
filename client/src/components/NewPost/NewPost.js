df020f3385d435be08e5b3ebf194b29805cf79dc
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-05-28 22:33:18 -0700

    CreatePost page (frontend only) should be mostly completed. Created a NewPost.js component that serves as a form for creating and publishing a post.

--- client/src/components/NewPost/NewPost.js
+++ client/src/components/NewPost/NewPost.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Select from "react-select";
 import "./NewPost.css";
+import axios from "axios";
 
-function NewPost({ addComment, depth }) {
+function NewPost() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const [file, setFile] = useState("");
+  const [fileName, setFileName] = useState("");
+  const [uploadedFile, setUploadedFile] = useState({});
+  const fileRef = useRef(); //for resetting the file input; reference: https://stackoverflow.com/a/60986322/16923546
 
   const tags = [
     { value: "food", label: "Food" },
@@ -12,27 +19,58 @@ function NewPost({ addComment, depth }) {
     { value: "arts", label: "Arts" },
   ];
 
-  function handleSubmit() {
+  //Reference for file submission: https://youtu.be/b6Oe2puTdMQ
+  function handleFileChange(event) {
+    //using JavaScript's File API
+    setFile(event.target.files[0]); //getting the first file from the input element of type 'files'
+    setFileName(event.target.files[0].name); //the file's name as a read-only string.
+  }
+
+  async function handleSubmit(event) {
+    console.log("SELECTED TAGS", selectedTags);
     if (title.trim() === "") {
       return;
     }
     setTitle("");
     setDescription("");
     console.log(description, title, " ");
-  }
+    setSelectedTags([]); //reset tag selection on form submit (i.e. submitting the post)
+    setFileName("");
+    event.preventDefault();
 
-  function handleTags(selectedTags) {
-    console.log("handleTags", selectedTags);
+    const formData = new FormData(); //add the file to FormData
+    formData.append("file", file);
+
+    try {
+      //Make an http POST request to pass the file to the server/backend
+      const res = await axios.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      const { fileName, filePath } = res.data; //the response we send at the end of the POST request for /upload: `res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });`
+      setUploadedFile({ fileName, filePath }); //MIGHT DELETE THIS
+      fileRef.current.value = ""; //for resetting the file input
+    } catch (err) {
+      if (err.response.status === 500) {
+        console.log("There was a problem with the server."); //if we're unable to find the file path
+      } else {
+        console.log(err.response.data.mesg); //if no file is uploaded
+      }
+    }
   }
 
   return (
     <div id="mainContainer">
       <div id="postContainer">
+        {/* For updating selected tags in form: https://stackoverflow.com/a/66257095/16923546 */}
         <Select
           id="selectTags"
           placeholder="Select tags..."
+          value={selectedTags}
           options={tags}
-          onChange={handleTags}
+          onChange={setSelectedTags}
           isMulti
         />
         <div id="inputs">
@@ -54,8 +92,27 @@ function NewPost({ addComment, depth }) {
             onChange={(e) => setDescription(e.target.value)}
           ></input>
         </div>
+        <div id="fileContainer">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="file"
+              ref={fileRef}
+              onChange={handleFileChange}
+              id="fileInput"
+              className="hideUploadButton"
+            />
+            <label htmlFor="fileInput" id="uploadButton">
+              Upload File
+            </label>
+            <p>{fileName}</p>
+            <button id="submit" className="hideUploadButton" type="submit">
+              {/* id='submit' allows this input file form to be also submitted for when the Post button is pressed ;reference: https://stackoverflow.com/a/53573760/16923546*/}
+            </button>
+          </form>
+        </div>
         <div id="submission">
-          <button id="submit" onClick={handleSubmit}>
+          <button form="submit" className="submitButton" onClick={handleSubmit}>
+            {/* form='submit' allows the Post button to be linked to the fileUploadButton*/}
             Post
           </button>
         </div>
--- server/app.js
+++ server/app.js
@@ -1,8 +1,43 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
 const app = express();
 const port = 5000;
 
+app.use(fileUpload());
+
 // Set up routes and middleware here
+app.post("/upload", (req, res) => {
+  if (req.files === null) {
+    //checking if file input is empty
+    return res.status(400).json({ mesg: "No file uploaded" });
+  }
+
+  console.log("wat", req.files.file);
+  const file = req.files.file; //containing the actual file and its raw data; Object is like: `file: { name: 'name.jpg', data: <Buffer ff blah blah>, size: #, mimetype: 'image/jpeg', etc.}`
+
+  const filePath = path.join(
+    __dirname,
+    "../",
+    "client",
+    "public",
+    "uploads",
+    file.name
+  );
+  // console.log(filePath); used this to see the filepath for debugging purposes
+
+  //TODO: Here we can send the file to the MongoDB database instead of storing it in the uploads folder.
+  //storing file into client/public/uploads folder
+  file.mv(filePath, (err) => {
+    if (err) {
+      console.error(err); //if path does not exist, log the error to console
+      return res.status(500).send(err);
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
