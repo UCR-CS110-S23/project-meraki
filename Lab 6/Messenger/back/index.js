59cce324712f09181baa6684d7c9ab7260338d38
ConnieP01 <57119043+ConnieP01@users.noreply.github.com>
2023-05-30 15:30:29 -0700

    Update index.js

--- Lab 6/Messenger/back/index.js
+++ Lab 6/Messenger/back/index.js
@@ -9,6 +9,8 @@ const session = require('express-session');
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bodyParser = require( 'body-parser');
+const auth = require('./routes/auth');
+const rooms = require('./routes/rooms');
 
 const app = express(); 
 const server = http.createServer(app);
@@ -23,54 +25,25 @@ const io = socketIO(server, {
 });
 app.use(cors({origin: 'http://localhost:3000', credentials:true}))
 
+dotenv.config();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Connect to the database
+// TODO: your code here
 mongoose.connect(process.env.MONGO_URL);
 const database = mongoose.connection;
 database.on('error', (error) => console.error(error));
 database.once('open', () => console.log('Connected to Database'));
 
+// Set up the session
+// TODO: your code here
 const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.SESSION_SECRET,
 })
 app.use(sessionMiddleware);
-
-io.use((socket, next) => {
-  sessionMiddleware(socket.request, {}, next);
-});
-
-io.use((socket, next) => {
-  //check if the user is authenticated
-  if (socket.request.session && socket.request.session.authenticated) {
-    next();
-  } else {
-    console.log("unauthorized")
-    next(new Error('unauthorized'));
-  }
-});
-
-
-
-dotenv.config();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-
-// Connect to the database
-// TODO: your code here
-
-
-
-// Set up the session
-// TODO: your code here
-
-
-
-
-const routes = require('./routes/auth');
-const rooms = require('./routes/rooms');
-
 
 app.get('/', (req, res) => {
   if (req.session && req.session.authenticated) {
@@ -83,7 +56,7 @@ app.get('/', (req, res) => {
 });
 
 
-app.use("/api/auth/", routes);
+app.use("/api/auth/", auth);
 
 
 // checking the session before accessing the rooms
@@ -106,8 +79,20 @@ server.listen(process.env.PORT, () => {
 
 // TODO: make sure that the user is logged in before connecting to the socket
 // TODO: your code here
+io.use((socket, next) => {
+  console.log("socket to middleware")
+  sessionMiddleware(socket.request, {}, next);
+});
 
-
+io.use((socket, next) => {
+  //check if the user is authenticated
+  if (socket.request.session && socket.request.session.authenticated) {
+    next();
+  } else {
+    console.log("unauthorized")
+    next(new Error('unauthorized'));
+  }
+});
 
 io.on('connection', (socket)=>{
   console.log("user connected")
