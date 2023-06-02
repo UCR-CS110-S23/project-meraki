bcecd7b60a5c0eb56f13015b6492409943638e00
hbach24 <57569284+hbach24@users.noreply.github.com>
2023-06-01 23:57:15 -0700

    Register form registers the user to MongoDB database when they hit submit. Did this by making a POST request in Auth.js (frontend) which sends the user's input under 'body: ' in the request. This request goes to '/register' POST request function in auth.js (backend) where we save() the user to the database. index.js just has added comments.

--- Lab 6/Messenger/back/routes/auth.js
+++ Lab 6/Messenger/back/routes/auth.js
@@ -1,30 +1,50 @@
-const express = require('express');
-const User = require('../model/user');
-const router = express.Router()
+const express = require("express");
+const User = require("../model/user");
+const router = express.Router();
 
 module.exports = router;
 
-router.post('/login', async (req, res) => {
-    const {session} = req;
-    const { username, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { session } = req;
+  const { username, password } = req.body;
 
-    // check if user in database
-    const user = await User.findOne({ username });
-    
-    if (!user)
-      return res.json({ msg: "Incorrect Username ", status: false });
-    else if (user.password !== password)
-      return res.json({ msg: "Incorrect Password", status: false });
-    else {
-      session.authenticated = true;
-      session.username = username;
-      res.json({ msg: "Logged in", status: true });
-    }
+  // check if user in database
+  const user = await User.findOne({ username });
+
+  if (!user) return res.json({ msg: "Incorrect Username ", status: false });
+  else if (user.password !== password)
+    return res.json({ msg: "Incorrect Password", status: false });
+  else {
+    session.authenticated = true;
+    session.username = username;
+    res.json({ msg: "Logged in", status: true });
+  }
 });
 
 // Set up a route for the logout page
-router.get('/logout', (req, res) => {
-    // Clear the session data and redirect to the home page
-    req.session.destroy();
-    res.send({msg: "Logged out", status: true})
+router.get("/logout", (req, res) => {
+  // Clear the session data and redirect to the home page
+  req.session.destroy();
+  res.send({ msg: "Logged out", status: true });
+});
+
+//TODO: ADD CODE FOR REGISTER PAGE
+router.post("/register", async (req, res) => {
+  const { username, password, name } = req.body;
+  console.log(username, password, name);
+
+  const user = new User({
+    username: username,
+    password: password,
+    name: name,
+    rooms: [],
   });
+
+  try {
+    const dataSaved = await user.save();
+    //passes user (username, password, name, rooms) as json to Auth.js as a response
+  } catch (error) {
+    console.log(error);
+    res.send("ERROR!");
+  }
+});
--- Lab 6/Messenger/front/src/Screens/Auth.js
+++ Lab 6/Messenger/front/src/Screens/Auth.js
@@ -2,54 +2,98 @@ import react from "react";
 import Form from "../Components/form.js";
 import { Button } from "@mui/material";
 
-class Auth extends react.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            showForm: false,
-            selectedForm: undefined,
-        }
-    }
+class Auth extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false,
+      selectedForm: undefined,
+    };
+  }
 
-    closeForm = () => {
-        this.setState({showForm: false});
-    }
+  closeForm = () => {
+    this.setState({ showForm: false });
+  };
 
-    login = (data) => {
-        // TODO: write codes to login
-    }
+  login = (data) => {
+    // TODO: write codes to login
+  };
 
-    register = (data) => {
-        // TODO: write codes to register
-        console.log(data);
-    }
+  register = (data) => {
+    // TODO: write codes to register
+    fetch(this.props.server_url + "/api/auth/register", {
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) =>
+      //once we get the response from the POST request, we can process sent response's data from `res.status(200).json(dataSaved);`
+      res.json().then((data) => {
+        console.log(data); //viewing the inputted user login info on browser's console
+      })
+    );
+  };
 
-    render(){
-        let display = null;
-        if (this.state.showForm){
-            let fields = [];
-            if (this.state.selectedForm === "login"){
-                fields = ['username', 'password'];
-                display = <Form fields={fields} close={this.closeForm} type="login" submit={this.login} key={this.state.selectedForm}/>;
-            }
-            else if (this.state.selectedForm === "register"){
-                fields = [ 'username', 'password', 'name'];
-                display = <Form fields={fields} close={this.closeForm} type="register" submit={this.register} key={this.state.selectedForm}/>;
-            }   
-        }
-        else{
-            display = <div>
-                <Button onClick={() => this.setState({showForm: true, selectedForm:"login"})}> Login </Button>
-                <Button onClick={() => this.setState({showForm: true, selectedForm: "register"})}> Register </Button>
-                </div>              ;
-        }
-        return(
-            <div>
-                <h1> Welcome to our website! </h1>
-                {display}
-            </div>
+  render() {
+    let display = null;
+    if (this.state.showForm) {
+      let fields = [];
+      if (this.state.selectedForm === "login") {
+        fields = ["username", "password"];
+        display = (
+          <Form
+            fields={fields}
+            close={this.closeForm}
+            type="login"
+            submit={this.login}
+            key={this.state.selectedForm}
+          />
         );
+      } else if (this.state.selectedForm === "register") {
+        fields = ["username", "password", "name"];
+        display = (
+          <Form
+            fields={fields}
+            close={this.closeForm}
+            type="register"
+            submit={this.register}
+            key={this.state.selectedForm}
+          />
+        );
+      }
+    } else {
+      display = (
+        <div>
+          <Button
+            onClick={() =>
+              this.setState({ showForm: true, selectedForm: "login" })
+            }
+          >
+            {" "}
+            Login{" "}
+          </Button>
+          <Button
+            onClick={() =>
+              this.setState({ showForm: true, selectedForm: "register" })
+            }
+          >
+            {" "}
+            Register{" "}
+          </Button>
+        </div>
+      );
     }
+    return (
+      <div>
+        <h1> Welcome to our website! </h1>
+        {display}
+      </div>
+    );
+  }
 }
 
 export default Auth;
